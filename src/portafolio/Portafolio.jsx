61f31eecd61d79bd4827db5bc8d4937907f9d75e
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-01-03 21:04:14 -0300

    cnc_backend

--- src/portafolio/Portafolio.jsx
+++ src/portafolio/Portafolio.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import axios from "axios"
 import "./Portafolio.css"
 
 
 export default function Portafolio() {
-    
+    const[projects, setProjects] = useState([])
+    useEffect(() => {
+        axios.get('https://backend-portafolio.herokuapp.com/portafolio')
+            .then(res => setProjects(res.data))            
+    },[setProjects])
     
     return (
-        <div>
-            
+        <div className="container-portafolio">
+            {projects && projects.map((py) => 
+                <div className="card-portafolio">
+                    <h1>{py.name}</h1>
+                    <img 
+                    src={py.image}
+                    alt="" 
+                    className="img-proyecto"/>
+                    <p>{py.descripcion}</p>
+                    <button><a href={py.link}>Demo</a></button>
+                    
+                </div>
+            )}  
         </div>
     )
 }
